9a4201d56917b46f19d35397d2dbcd32f3110764
Empiric-52 <Empiric-52@DESKTOP-O2G7T0E>
2024-01-29 17:45:22 +0530

    adding

--- GitHubProject1/src/GitHubProject1Package/GitHubProject1Class.java
+++ GitHubProject1/src/GitHubProject1Package/GitHubProject1Class.java
@@ -5,6 +5,7 @@ public class GitHubProject1Class {
 	public static void main(String[] args) {
 		System.out.println("Hellow GitHubProject1 Class");
 
+		System.out.println("Add for cmd git gui command");
 	}
 
 }
